7245cbcb9c8f11c34cb2fa39153fba9e3ded5407
Salvatore Barretta <125928617+Sasynovic@users.noreply.github.com>
2024-09-26 23:48:11 +0200

    uploading js files

---
+++ js/frame.js
@@ -0,0 +1,15 @@
+document.addEventListener('DOMContentLoaded', function() {
+    const motto = document.querySelector('.motto');
+
+    const frame = document.querySelector('.frame');
+    const body = document.querySelector('body');
+
+    // Disabilita lo scroll quando la pagina viene caricata
+    body.classList.add('no-scroll');
+
+    motto.addEventListener('animationend', function () {
+        frame.classList.add('shrink-frame');
+        // Riabilita lo scroll dopo che il video e il frame si sono rimpiccioliti
+        body.classList.remove('no-scroll');
+    });
+});
