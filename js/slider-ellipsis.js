015a6702ce30563d64218ebc95547218c54d9f91
Salvatore Barretta <barrettasalvatore@outlook.it>
2024-09-28 20:16:56 +0200

    correzioni generali

--- js/slider-ellipsis.js
+++ js/slider-ellipsis.js
@@ -2,8 +2,22 @@ document.addEventListener('DOMContentLoaded', function() {
     const paragraphs = document.querySelectorAll('.description');
     const h1 = document.querySelectorAll('h2');
 
-    const maxLengthP = 500;
-    const maxLengthH1 = 23;
+    let maxLengthP;
+    let maxLengthH1;
+
+    if (window.innerWidth <= 600) {
+        // Schermi piccoli (smartphone)
+        maxLengthP = 480;
+        maxLengthH1 = 23;
+    } else if (window.innerWidth <= 1024) {
+        // Schermi medi (tablet)
+        maxLengthP = 1500;
+        maxLengthH1 = 40;
+    } else {
+        // Schermi grandi (desktop)
+        maxLengthP = 1700;
+        maxLengthH1 = 45;
+    }
 
     paragraphs.forEach((paragraph, index) => {
         if (paragraph.textContent.length > maxLengthP) {
