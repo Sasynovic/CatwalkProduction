1aa1219cd216269d7aeb689a1dc70b3b43df0867
Salvatore Barretta <barrettasalvatore@outlook.it>
2024-09-30 23:26:45 +0200

    fix : fixing and testing

--- js/frame.js
+++ js/frame.js
@@ -1,16 +1,15 @@
 document.addEventListener('DOMContentLoaded', function() {
-    const motto = document.querySelector('.motto');
-
     const frame = document.querySelector('.frame');
     const body = document.querySelector('body');
 
     // Disabilita lo scroll quando la pagina viene caricata
     body.classList.add('no-scroll');
 
-    motto.addEventListener('animationend', function () {
+    // Imposta un timer per far partire l'animazione di shrinking del frame
+    setTimeout(() => {
         frame.classList.add('shrink-frame');
-        // Riabilita lo scroll dopo che il video e il frame si sono rimpiccioliti
-        body.classList.remove('no-scroll');
-    });
-});
 
+        // Riabilita lo scroll dopo l'inizio dell'animazione di shrinking
+        body.classList.remove('no-scroll');
+    }, 4000); // Imposta il tempo in millisecondi (qui 4 secondi)
+});
